a9f4177b5f8f89c217b8e0fef9a683d7aee1cadc
Neha-Mazumder <120391351+Neha-Mazumder@users.noreply.github.com>
2026-04-22 13:30:20 +0600

    backend
    database

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\flutter\\flutter_windows_3.35.5-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Youtech BD\\OneDrive\\Desktop\\Middy\\flutter_application_1" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\flutter\\flutter_windows_3.35.5-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\Youtech BD\\OneDrive\\Desktop\\Middy\\flutter_application_1"
+  "FLUTTER_ROOT=D:\\flutter\\flutter_windows_3.35.5-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Youtech BD\\OneDrive\\Desktop\\Middy\\flutter_application_1\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Youtech BD\\OneDrive\\Desktop\\Middy\\flutter_application_1"
+  "FLUTTER_TARGET=C:\\Users\\Youtech BD\\OneDrive\\Desktop\\Middy\\flutter_application_1\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzguNQ==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049ZjZmZjE1MjlmZA==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049MTUyN2FlMGVjNQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMC40"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Youtech BD\\OneDrive\\Desktop\\Middy\\flutter_application_1\\.dart_tool\\package_config.json"
+)
